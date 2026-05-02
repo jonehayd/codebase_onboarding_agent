fe697205699243271bcea384f7b3fe13a5493126
Hayden Jones <jonehm05@pfw.edu>
2026-05-02 18:35:58 -0400

    feat: implement IngestionPage with session management and progress tracking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import IngestionPage from "./pages/IngestionPage";
 
 export default function App() {
-  return <LoginPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/sessions/:sessionId/ingesting" element={<IngestionPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
